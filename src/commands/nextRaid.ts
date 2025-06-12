8dd4c10f4e087ef351d0a9e5c28f0c33ea3fd576
Nick van Bragt <nick.van.bragt@hva.nl>
2025-06-12 16:03:58 +0200

    chore: f the yanks

--- src/commands/nextRaid.ts
+++ src/commands/nextRaid.ts
@@ -1,18 +1,38 @@
-import {CommandInteraction, PermissionFlagsBits, SlashCommandBuilder} from "discord.js";
+import {CommandInteraction, SlashCommandBuilder} from "discord.js";
 import botVars from "../bot";
+
 export const data = new SlashCommandBuilder()
     .setName('nextraid')
     .setDescription('Returns the next raid date');
+
 export async function execute(interaction: CommandInteraction) {
     const nextRaidDate = determineNextRaid();
-    await interaction.reply(`The next raid is at: ${nextRaidDate}`);
+    await interaction.reply(`The next raid will be on: ${nextRaidDate}`);
 }
 
 function determineNextRaid() {
     const invReminder = botVars.scheduler.getInviteReminder();
-    if (invReminder.isActive) {
-        return botVars.scheduler.getInviteReminder().nextDate().toFormat("dd/MM");
-    } else {
-        return botVars.scheduler.getInviteStaller()?.nextDate().toFormat("dd/MM");
+    const date = invReminder.isActive ? invReminder.nextDate() : botVars.scheduler.getInviteStaller()?.nextDate();
+    if (!date) {
+        throw Error("Ah oh spaghetti-o, this should never happen. Couldn't fetch date for next raid");
     }
+    const day = getDateOrdinal(date.get("day"));
+    const month = date.monthLong;
+    return `${day} of ${month}`;
+}
+
+/**
+ * Yanks apparently can't read dd/MM
+ */
+function getDateOrdinal(day: number) {
+    let suffix = 'th';
+    if (day % 10 === 1 && day !== 11) {
+        suffix = 'st';
+    } else if (day % 10 === 2 && day !== 12) {
+        suffix = 'nd';
+    } else if (day % 10 === 3 && day !== 13) {
+        suffix = 'rd';
+    }
+
+    return day + suffix;
 }
