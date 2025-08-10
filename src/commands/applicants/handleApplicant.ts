8a18d63b73b26748449f59541d15cb875eefe300
Nick van Bragt <nick.van.bragt@hva.nl>
2025-08-10 20:00:10 +0200

    add clarification for applicant when denied

--- src/commands/applicants/handleApplicant.ts
+++ src/commands/applicants/handleApplicant.ts
@@ -4,7 +4,7 @@ import {
     PermissionFlagsBits,
     SlashCommandBuilder,
     TextChannel,
-    OverwriteType, Guild, Role
+    OverwriteType, Guild, Role, ChatInputCommandInteraction
 } from "discord.js";
 import {config} from "../../config";
 
@@ -18,8 +18,14 @@ export const data = new SlashCommandBuilder()
             .setDescription('True to accept, false to deny')
             .setRequired(true)
     )
+    .addStringOption(option =>
+        option
+            .setName('message')
+            .setDescription('Message that is sent to the applicant when denied')
+            .setRequired(false)
+    )
 
-export async function execute(interaction: CommandInteraction) {
+export async function execute(interaction: ChatInputCommandInteraction) {
     console.log("executing");
     const member = interaction.member as GuildMember;
     const allowedRoles = ['GM', 'Assistant GM', 'Recruitment'];
@@ -27,11 +33,10 @@ export async function execute(interaction: CommandInteraction) {
     if (!interaction.guild || intersection.size === 0) {
         return interaction.reply(`You're not allowed to do this.`);
     }
-
+    await interaction.deferReply({ ephemeral: true });
     const applicantHandled = await handleApplicant(interaction);
     if (!applicantHandled) {
-        console.log("DId not get handled", applicantHandled);
-        await interaction.reply({content: "This command can only be ran in an applicant channel", ephemeral: true});
+        await interaction.editReply({content: "This command can only be ran in an applicant channel"});
     }
 }
 
@@ -46,7 +51,7 @@ export async function findApplicant(channel: TextChannel) {
     return channel.guild.members.cache.get(memberOverwrite.id) || false;
 }
 
-async function handleApplicant(interaction: CommandInteraction) {
+async function handleApplicant(interaction: ChatInputCommandInteraction) {
     const channel = interaction.channel as TextChannel;
     if (channel.parent?.name !== "ꓮpplicants") {
         return false;
@@ -55,13 +60,13 @@ async function handleApplicant(interaction: CommandInteraction) {
     const applicant = await findApplicant(channel);
     if (!applicant) {
         console.log("Couldn't find applicant");
-        await interaction.reply({content: "done", ephemeral: true});
+        await interaction.editReply("done");
         await channel.delete("Applicant no longer exists");
         return true;
     }
 
-    // @ts-ignore
     const accepted = interaction.options.getBoolean("accept");
+    const message = interaction.options.getString("message");
     await interaction.guild?.roles.fetch()
     const raiderRole = interaction.guild?.roles.cache.find(role => role.name === config.RAIDER_ROLE_NAME);
     const applicantRole = interaction.guild?.roles.cache.find(role => role.name === config.APPLICANT_ROLE_NAME) as Role;
@@ -73,11 +78,13 @@ async function handleApplicant(interaction: CommandInteraction) {
     }
 
     if (!accepted) {
-        await applicant.kick("Sorry, your application has been denied");
+        const baseMessage = 'Hi, I\'m sorry to inform you that you\'ve been declined for your application to RAVAGE.';
+        await applicant.send(message ? baseMessage + 'Reason: ' + message : baseMessage);
+        await applicant.kick();
     }
 
     console.log("All done, deleting channel");
-    await interaction.reply({content: "done", ephemeral: true});
+    await interaction.editReply("done");
     await channel.delete("Applicant no longer exists");
     return true;
 }
